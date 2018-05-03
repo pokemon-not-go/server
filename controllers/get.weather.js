d7423693ea4b7d0dd4781d7f1584666042dc556b
Natascha Lamsu <tazcha15@gmail.com>
2018-05-03 21:52:52 +0700

    edit

--- controllers/get.weather.js
+++ controllers/get.weather.js
@@ -1,10 +1,17 @@
 const axios = require('axios')
 
-axios.get('http://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=bc8acfa8f52dbc5fee477318284b9856')
+
+module.exports = {
+  getWeather: (req, res) => {
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=bc8acfa8f52dbc5fee477318284b9856')
    .then(function(response) {
      let currentWeather = response.data.weather[0].main
-      console.log(currentWeather)
+      res
+        .status(200)
+        .send(currentWeather)
    })
    .catch(function(err) {
       console.log(err)
    })
+  }
+}
