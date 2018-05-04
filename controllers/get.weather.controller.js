3fb0e6ffb0b12a7fd31d8c521f4670f74594ce17
Natascha Lamsu <tazcha15@gmail.com>
2018-05-04 11:33:18 +0700

    fixing conflict

--- controllers/get.weather.controller.js
+++ controllers/get.weather.controller.js
@@ -1,17 +1,19 @@
-const axios    = require('axios')
+const axios = require('axios')
+
 
 module.exports = {
   getWeather: (req, res) => {
     axios.get('http://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=bc8acfa8f52dbc5fee477318284b9856')
-      .then((response) => {
-        res.send(200).json({
-          currentWeather: response.data.weather[0].main
-        })
-      })
-      .catch((err) => {
-        res.send(400).json({
-          err
-        })
-      })
+   .then(function(response) {
+     let currentWeather = response.data.weather[0].main
+      res
+        .status(200)
+        .send(currentWeather)
+   })
+   .catch(function(err) {
+      res
+        .status(400)
+        .send(err)
+   })
   }
 }
