074b3d3bb243c1e867ca70d705cc8a3b9892b1f8
JoviAndrew <jovian_andrew@hotmail.com>
2018-05-04 13:29:35 +0700

    able to update user data and get weather data

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user_model')
 const jwt = require('jsonwebtoken')
+const axios = require('axios')
 
 module.exports = {
   loginUser (req, res) {
@@ -22,7 +23,8 @@ module.exports = {
           let token = jwt.sign({userId: response._id, name: response.name}, process.env.SECRET)
           res.status(200).json({
             message: 'Success create new user',
-            data: response
+            data: response,
+            token: token
           })
         })
         .catch(function(err){
@@ -42,6 +44,38 @@ module.exports = {
     .catch(function(err){
       res.status(500).json({
         message: 'error finding user',
+        error: err
+      })
+    })
+  },
+  getWeather: (req, res) => {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=${process.env.TOKEN_WEATHER}`)
+      .then((response) => {
+        res.status(200).json({
+          message: 'success get weather detail',
+          currentWeather: response.data.weather[0].main
+        })
+      })
+      .catch((err) => {
+        res.status(400).json({
+          err
+        })
+      })
+  },
+  catchPokemon (req, res) {
+    let pokemon = req.body.pokemon
+    let userId = req.body.id
+
+    User.updateOne({_id: id}, {pokemon: pokemon})
+    .then(function(result){
+      res.status(200).json({
+        message: 'Success capture a pokemon',
+        result: result
+      })
+    })
+    .catch(function(err){
+      res.status(500).json({
+        message: 'error occured when updating data',
         error: err
       })
     })
--- routers/index_routers.js
+++ routers/index_routers.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
-const { loginUser } = require('../controllers/index_controller')
+const { loginUser, getWeather, catchPokemon } = require('../controllers/index_controller')
 
 router.post('/login', loginUser)
+
+router.post('/weather', getWeather)
+
+router.put('/catch', catchPokemon)
 
 module.exports = router
